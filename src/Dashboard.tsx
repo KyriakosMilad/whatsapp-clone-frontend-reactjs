ef5da06c64093c8c6b841a4112bcf277e5ab7eb7
Kyriakos Milad <kero.cool16@gmail.com>
2020-09-10 09:32:10 +0200

    Create sidebar component

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
+import Sidebar from './Sidebar';
 
 export default class Dashboard extends Component {
 	render() {
 		return (
 			<Container fluid>
 				<Row>
-					<Col md={2}>1 of 1</Col>
+					<Sidebar />
 					<Col md={10}>1 of 1</Col>
 				</Row>
 			</Container>
---
+++ src/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Col} from 'react-bootstrap';
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <Col md={2}>2col</Col>
+    )
+  }
+}
