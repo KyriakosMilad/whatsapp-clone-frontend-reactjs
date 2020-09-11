97b19f94669015980a46087af9b9516564b14b38
Kyriakos Milad <kero.cool16@gmail.com>
2020-09-11 10:36:25 +0200

    Fix button sizes on very small screens and add border to chat card

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -24,10 +24,10 @@ export default class Sidebar extends Component {
 					/>
 				</div>
 				<Row className="buttonsRow mx-0">
-					<Col sm={6} className="px-0">
+					<Col xs={6} className="px-0">
 						<Button type="button" className="w-100 newChatButton">New chat</Button>
 					</Col>
-					<Col sm={6} className="px-0">
+					<Col xs={6} className="px-0">
 						<Button type="button" variant="success" className="w-100 newContactButton">New contact</Button>
 					</Col>
 				</Row>
